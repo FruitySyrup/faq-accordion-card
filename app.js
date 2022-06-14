8e9912b05fbb06c06a90368107e56418b3af5d78
FruitySyrup <106176876+FruitySyrup@users.noreply.github.com>
2022-06-14 17:15:23 -0400

    added open/close

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// const accordion = document.querySelectorAll ('.container');
-// const questionTag = document.querySelectorAll ('.question');
-// const answerTag = document.querySelector ('.answer')
-
-// for (i=0; i<accordion.length; i++) {
-//   questionTag[i].addEventListener('click', function () {
-    // answerTag.classList.toggle('open')
-//   })
-// }
-
 const questionTag = document.querySelectorAll('.question')
 
 questionTag.forEach(questionTag => {
     questionTag.addEventListener('click', () => {
-        questionTag.classList.toggle('open')
-    })
-})
+        if (questionTag.classList.contains('open')) {
+            questionTag.classList.remove('open');
+        } else {
+            const questionTagOpen = document.querySelectorAll('.open');
+
+            questionTagOpen.forEach((questionTagOpen) => {
+            questionTagOpen.classList.remove('open');
+            });
+            questionTag.classList.add('open');
+        }
+    });
+});
