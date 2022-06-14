dfda9e8bdbdcb8aebb0c2e53dc5f3cc2d2cedd45
FruitySyrup <106176876+FruitySyrup@users.noreply.github.com>
2022-06-13 20:55:37 -0400

    trying

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const accordion = document.querySelectorAll ('.container');
-const questionTag = document.querySelectorAll ('.question');
-const answerTag = document.querySelector ('.answer')
+// const accordion = document.querySelectorAll ('.container');
+// const questionTag = document.querySelectorAll ('.question');
+// const answerTag = document.querySelector ('.answer')
 
+// for (i=0; i<accordion.length; i++) {
+//   questionTag[i].addEventListener('click', function () {
+    // answerTag.classList.toggle('open')
+//   })
+// }
 
-for (i=0; i<accordion.length; i++) {
-  questionTag[i].addEventListener('click', function () {
-    answerTag.classList.toggle('open')
-  })
+const containerTag = document.querySelector ('.container')
+var i 
+
+for (i=0; i< containerTag.length; i++) {
+    containerTag[i].addEventListener('click', function (){
+        this.classList.toggle('open');
+    })
 }
