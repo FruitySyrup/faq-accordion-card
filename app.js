aa0fd484408d256ed36e84fc7c579bbbbd33d1ba
FruitySyrup <106176876+FruitySyrup@users.noreply.github.com>
2022-06-22 16:44:18 -0400

    template

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-const questionTag = document.querySelectorAll('.question')
+//Order of code steps
+//1. Grab our items
+const items = [...document.querySelectorAll('li.faq__faq-item')]
+//2. Add event listeners to our items that handle an item click.
+//3. handleItemClick function -> 
+//3.1 Grab the clicked item
+//3.2 Grab the itemDetail as we need to add maxHeight
+//3.3 Check if item has .active class
+//3.4 If it does, remove the maxHeight and remove the .active class
+//3.5 If it doesn't, add the maxHeight and add the .active class
 
-questionTag.forEach(questionTag => {
-    questionTag.addEventListener('click', () => {
-        if (questionTag.classList.contains('open')) {
-            questionTag.classList.remove('open');
-        } else {
-            const questionTagOpen = document.querySelectorAll('.open');
+const clearItem = (itemDetail) => {
+    itemDetail.style.maxHeight = null
+}
 
-            questionTagOpen.forEach((questionTagOpen) => {
-            questionTagOpen.classList.remove('open');
-            });
-            questionTag.classList.add('open');
-        }
-    });
-});
+const activateItem = (itemDetail) => {
+    const scrollHeight = itemDetail.scrollHeight
+    itemDetail.style.maxHeight = `${scrollHeight}px`
+}
+
+const handleItemClick = (e) => {
+    const clickedItem = e.target.closest('li')
+    const itemDetail = clickedItem.querySelector('.faq-item__detail')
+    const clickedItemActive = clickedItem.classList.contains('active')
+    clickedItem.classList.toggle('active')
+    if (clickedItemActive) {
+        clearItem(itemDetail)
+    } else {
+        activateItem(itemDetail)
+    }
+};
+
+const addListenersToItems = (array, callback) => {
+    array.forEach(item => {
+        item.addEventListener('click', (e) => {
+            callback(e)
+        })
+    })
+};
 
 
-const arrowTag = document.querySelectorAll('img.arrow')
+addListenersToItems(items, handleItemClick)
 
-questionTag.addEventListener('click', () => {
-        arrowTag.classList.toggle('rotate');
-    });
+items[0].click()
