1590d67a1ed97159b0391d4ee3dd5affa4f93b9f
FruitySyrup <106176876+FruitySyrup@users.noreply.github.com>
2022-06-26 19:57:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,41 +4,22 @@ const items = [...document.querySelectorAll('li.faq_faq-item')]
 //2. Add event listeners to our items that handle an item click.
 //3. handleItemClick function -> 
 //3.1 Grab the clicked item
-//3.2 Grab the itemDetail as we need to add maxHeight
+//3.2 Grab the itemAnswer as we need to add maxHeight
 //3.3 Check if item has .active class
 //3.4 If it does, remove the maxHeight and remove the .active class
 //3.5 If it doesn't, add the maxHeight and add the .active class
 
-const clearItem = (itemDetail) => {
-    itemDetail.style.maxHeight = null
-}
-
-const activateItem = (itemDetail) => {
-    const scrollHeight = itemDetail.scrollHeight
-    itemDetail.style.maxHeight = `${scrollHeight}px`
-}
-
-const handleItemClick = (e) => {
-    const clickedItem = e.target.closest('li')
-    const itemDetail = clickedItem.querySelector('.faq-item_answer')
-    const clickedItemActive = clickedItem.classList.contains('active')
-    clickedItem.classList.toggle('active')
-    if (clickedItemActive) {
-        clearItem(itemDetail)
-    } else {
-        activateItem(itemDetail)
+items.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const clickedItem = e.target.closest('li')
+        const itemAnswer = clickedItem.querySelector('.faq-item_answer')
+        const clickedItemActive = clickedItem.classList.contains('active')
+        clickedItem.classList.toggle('active')
+        if (clickedItemActive) {
+            itemAnswer.style.maxHeight = null
+        }  else {
+            const scrollHeight = itemAnswer.scrollHeight
+            itemAnswer.style.maxHeight = `${scrollHeight}px`
     }
-};
-
-const addListenersToItems = (array, callback) => {
-    array.forEach(item => {
-        item.addEventListener('click', (e) => {
-            callback(e)
-        })
     })
-};
-
-
-addListenersToItems(items, handleItemClick)
-
-items[0].click()
+})
